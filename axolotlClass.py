7c8798969643546baf0d5d655afa37960abed109
atletea <89878564+atletea@users.noreply.github.com>
2021-11-06 22:15:19 -0400

    Add files via upload

---
+++ axolotlClass.py
@@ -0,0 +1,17 @@
+class Axolotl(object):
+    def __init__(self):
+        self.color = "pink"
+        self.happiness = 100
+        self.hunger = 20
+        self.bestTime = None
+        # self.age
+    
+    def eatWorm(self):
+        return 42
+    
+    def changeColor(self):
+        return 42
+
+
+
+
